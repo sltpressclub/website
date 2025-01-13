acba7dfc5aeb5afa78f0e0d420c7375eb08b5601
Aman Kauroo <63089958+amankauroo@users.noreply.github.com>
2025-01-13 23:28:58 +0400

    24th version

--- pages/authors.js
+++ pages/authors.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import { Author } from "../components"; // Import the Author component
 import { getAuthors } from "../services"; // Import service to fetch authors
+import Loader from "../components/Loader"; // Import the Loader component
 
 const Authors = ({ authors }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Use effect to set loading state when authors data is available
+  useEffect(() => {
+    if (authors.length > 0) {
+      setIsLoading(false); // Hide loader when data is loaded
+    }
+  }, [authors]);
+
   return (
     <div className="flex flex-col min-h-screen w-full bg-transparent text-white">
       <Head>
@@ -15,11 +25,16 @@ const Authors = ({ authors }) => {
       <div className="w-full px-4 md:px-10 mb-8 flex-grow">
         <h1 className="text-4xl font-bold text-center mb-12">Our Authors</h1>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12">
-          {authors.map((author) => (
-            <Author key={author.id} author={author} />
-          ))}
-        </div>
+        {/* Display loader if data is still loading */}
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12">
+            {authors.map((author) => (
+              <Author key={author.id} author={author} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
