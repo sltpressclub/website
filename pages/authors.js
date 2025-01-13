fa1e113c015bce7f34048facb175b4d3575f4a5f
Aman Kauroo <63089958+amankauroo@users.noreply.github.com>
2025-01-13 23:19:12 +0400

    21th version

---
+++ pages/authors.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Head from "next/head";
+import { Author } from "../components"; // Import the Author component
+import { getAuthors } from "../services"; // Import service to fetch authors
+
+const Authors = ({ authors }) => {
+  return (
+    <div className="flex flex-col min-h-screen w-full bg-gray-900 text-white">
+      <Head>
+        <title>Authors | SLT Pressclub</title>
+        <link rel="icon" href="/slt_pressclub_logo.png" />
+      </Head>
+
+      {/* Author Grid */}
+      <div className="w-full px-4 md:px-10 mb-8 flex-grow">
+        <h1 className="text-4xl font-bold text-center mb-12">Our Authors</h1>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12">
+          {authors.map((author) => (
+            <Author key={author.id} author={author} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// Fetch all authors from GraphCMS
+export async function getStaticProps() {
+  const authors = (await getAuthors()) || []; // Fetch authors
+  return {
+    props: { authors },
+  };
+}
+
+export default Authors;
