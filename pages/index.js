916d083a5b42e2694a594dfc6e4e24ba3ca6c7f9
Aman Kauroo <63089958+amankauroo@users.noreply.github.com>
2025-01-29 18:35:07 +0400

    166th version

--- pages/index.js
+++ pages/index.js
@@ -45,9 +45,7 @@ export default function Home({ posts }) {
         <div className="mt-8">
           <Gallery />
         </div>
-        <div className="mt-12">
-          <Quotes />
-        </div>
+
         <div className="mt-8">
           <History />
         </div>
